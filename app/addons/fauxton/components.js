856c3b43262490988bd2764e90117911abc18d9d
Ben Keen <ben.keen@gmail.com>
2015-01-21 08:46:56 -0800

    sync

--- app/addons/fauxton/components.js
+++ app/addons/fauxton/components.js
@@ -112,6 +112,8 @@ function(app, FauxtonAPI, ReactComponents, ace, spin, ZeroClipboard) {
       // remove the current database name from the list
       dbNames = _.without(dbNames, currentDBName);
 
+      ViewEditor.renderEditor(this.el);
+
       /*
       this.lookaheadTray = this.insertView("#header-lookahead", new ReactComponents.LookaheadTray({
         data: dbNames,
